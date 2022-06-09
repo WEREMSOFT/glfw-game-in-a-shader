a57aa7292a5fbe466674f6d5ede0d07fbbbbd697
WEREMSOFT <pablo.weremczuk@gmail.com>
2022-06-09 22:59:02 +0000

    added diffuse color to shapes

--- assets/shader.fs
+++ assets/shader.fs
@@ -1,9 +1,10 @@
 
 #define MAX_STEPS 100
-#define MAX_DIST 50.
+#define MAX_DIST 100.
 #define SURF_DIST .01
 #define TAU 6.283185
 #define PI 3.141592
+
 struct Camera
 {
     vec3 position;
@@ -11,23 +12,35 @@ struct Camera
     vec3 lookAt;
 };
 
+struct CollisionInfo
+{
+    float distance;
+    vec4 color;
+};
+
+struct Shape
+{
+    vec3 position;
+    vec4 color;
+    vec3 rotation;
+};
+
 struct Torus
 {
+    Shape parent;
     vec2 radiouses;
-    vec3 position;
 };
 
 struct Box
 {
+    Shape parent;
     vec3 size;
-    vec3 position;
-    vec3 rotation;
 };
 
 struct Sphere
 {
+    Shape parent;
     float radius;
-    vec3 position;
 };
 
 mat2 getRotationMatrix(float angle)
@@ -43,81 +56,124 @@ float smoothMin(float a, float b, float ammount)
     return mix(b, a, h) - ammount * h * (1. - h);
 }
 
-float getSphereDistance(Sphere sphere, vec3 point)
+CollisionInfo getSphereDistance(Sphere sphere, vec3 point)
 {
-    return length(point - sphere.position) - sphere.radius;
+    CollisionInfo returnValue;
+    returnValue.color = sphere.parent.color;
+    returnValue.distance = length(point - sphere.parent.position) - sphere.radius;
+    return returnValue;
 }
 
-float getTorusDistance(Torus torus, vec3 point)
+CollisionInfo getTorusDistance(Torus torus, vec3 point)
 {
-    vec3 position = point - torus.position;
+    CollisionInfo returnValue;
+    returnValue.color = torus.parent.color;
+    vec3 position = point - torus.parent.position;
     float x = length(position.xz) - torus.radiouses.x;
-    return length(vec2(x, position.y)) - torus.radiouses.y;
+    returnValue.distance = length(vec2(x, position.y)) - torus.radiouses.y;
+    return returnValue;
 }
 
-float getBoxDistance(Box box, vec3 point)
+CollisionInfo getBoxDistance(Box box, vec3 point)
 {
+    CollisionInfo returnValue;
+    returnValue.color = box.parent.color;
     mat2 rotationMatrix = getRotationMatrix(iTime);
-    vec3 position = point - box.position;
+    vec3 position = point - box.parent.position;
     position.xz *= rotationMatrix;
-    return length(max(abs(position) - box.size, 0.));
+
+    returnValue.distance = length(max(abs(position) - box.size, 0.));
+    return returnValue;
 }
 
-float getDistance(vec3 point)
+CollisionInfo getDistance(vec3 point)
 {
+    CollisionInfo returnValue;
+    returnValue.color = vec4(1.);
+
     Sphere sphere;
-    sphere.position = vec3(0, 1., 6.);
+    sphere.parent.position = vec3(0, 1., 6.);
     sphere.radius = 1.;
+    sphere.parent.color = vec4(1., 1., 0, 1.);
 
     Torus torus;
     torus.radiouses = vec2(2., .3);
-    torus.position = vec3(-1., .5, 6.);
+    torus.parent.position = vec3(-1., .5, 6.);
+    torus.parent.color = vec4(1., 0, 0, 1.);
 
     Box box;
     box.size = vec3(.5);
-    box.position = vec3(-5., .5, 6.);
+    box.parent.position = vec3(-5., .5, 6.);
+    box.parent.color = vec4(1., 0, 1., 1.);
 
-    sphere.position.x = sin(iTime + 3.1416) * 3.;
-    sphere.position.z = cos(iTime + 3.1416) * 3. + 6.;
-    float distanceToSphere = getSphereDistance(sphere, point);
+    sphere.parent.position.x = sin(iTime + PI) * 3.;
+    sphere.parent.position.z = cos(iTime + PI) * 3. + 6.;
+    sphere.parent.color = vec4(1., 1., 0, 1.);
+    CollisionInfo distanceToSphere = getSphereDistance(sphere, point);
 
-    sphere.position.x = sin(iTime) * 3.;
-    sphere.position.z = cos(iTime) * 3. + 6.;
-    float distanceToSphere2 = getSphereDistance(sphere, point);
+    sphere.parent.position.x = sin(iTime) * 3.;
+    sphere.parent.position.z = cos(iTime) * 3. + 6.;
+    CollisionInfo distanceToSphere2 = getSphereDistance(sphere, point);
 
     torus.radiouses.x = abs(sin(iTime * .2)) + 0.5;
     torus.radiouses.y = abs(sin(iTime * 0.5)) + 0.1;
-    torus.position.y = abs(sin(iTime * 0.7)) + 0.5;
-    float distanceToTorus = getTorusDistance(torus, point);
+    torus.parent.position.y = abs(sin(iTime * 0.7)) + 0.5;
+    CollisionInfo distanceToTorus = getTorusDistance(torus, point);
     float distanceToPlane = point.y + 5. + sin(point.x * .5 + iTime) + cos(point.z * .5 + iTime);
 
-    float distanceToBox = getBoxDistance(box, point);
+    CollisionInfo distanceToBox = getBoxDistance(box, point);
 
-    return min(smoothMin(smoothMin(smoothMin(distanceToSphere, distanceToSphere2, .5), distanceToTorus, .5), distanceToBox, .5), distanceToPlane);
+    returnValue = distanceToSphere;
+
+    if (distanceToSphere.distance < distanceToSphere2.distance)
+    {
+        returnValue = distanceToSphere;
+    }
+
+    if (distanceToTorus.distance < returnValue.distance)
+    {
+        returnValue = distanceToTorus;
+    }
+
+    if (distanceToBox.distance < returnValue.distance)
+    {
+        returnValue = distanceToBox;
+    }
+
+    if (distanceToPlane < returnValue.distance)
+    {
+        returnValue.distance = distanceToPlane;
+        returnValue.color = vec4(1.);
+    }
+
+    // returnValue.distance = min(smoothMin(smoothMin(smoothMin(distanceToSphere.distance, distanceToSphere2.distance, .5), distanceToTorus.distance, .5), distanceToBox.distance, .5), distanceToPlane);
+    return returnValue;
 }
 
 vec3 getNormal(vec3 point)
 {
     vec2 e = vec2(.01, 0);
-    float distanceToPoint = getDistance(point);
+    CollisionInfo distanceToPoint = getDistance(point);
     vec3 n = vec3(
-        distanceToPoint - getDistance(point - e.xyy),
-        distanceToPoint - getDistance(point - e.yxy),
-        distanceToPoint - getDistance(point - e.yyx));
+        distanceToPoint.distance - getDistance(point - e.xyy).distance,
+        distanceToPoint.distance - getDistance(point - e.yxy).distance,
+        distanceToPoint.distance - getDistance(point - e.yyx).distance);
 
     return normalize(n);
 }
 
-float rayMarch(vec3 rayOrigin, vec3 rayDirection)
+CollisionInfo rayMarch(vec3 rayOrigin, vec3 rayDirection)
 {
-    float distanceToOrigin = 0;
+    CollisionInfo distanceToOrigin;
 
     for (int i = 0; i < MAX_STEPS; i++)
     {
-        vec3 point = rayOrigin + rayDirection * distanceToOrigin;
-        float distanceToScene = getDistance(point);
-        distanceToOrigin += distanceToScene;
-        if (distanceToOrigin > MAX_DIST || distanceToScene < SURF_DIST)
+        vec3 point = rayOrigin + rayDirection * distanceToOrigin.distance;
+        CollisionInfo distanceToScene = getDistance(point);
+
+        distanceToOrigin.distance += distanceToScene.distance;
+        distanceToOrigin.color = distanceToScene.color;
+        if (distanceToOrigin.distance > MAX_DIST || distanceToScene.distance < SURF_DIST)
             break;
     }
 
@@ -132,9 +188,9 @@ float getLight(vec3 point)
     vec3 n = getNormal(point);
     float diffuse = clamp(dot(n, lightPosNormalized), 0., 1.);
 
-    float d = rayMarch(point + n * SURF_DIST * 5., lightPosNormalized);
+    CollisionInfo d = rayMarch(point + n * SURF_DIST * 5., lightPosNormalized);
 
-    if (d < length(lightPosition - point))
+    if (d.distance < length(lightPosition - point))
         diffuse *= .1;
 
     return diffuse;
@@ -172,15 +228,17 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
 
     Camera camera = getCamera(m, uv);
 
-    float distance = rayMarch(camera.position, camera.direction);
+    CollisionInfo colInfo = rayMarch(camera.position, camera.direction);
 
     vec4 col = vec4(0);
 
-    if (distance < MAX_DIST)
+    if (colInfo.distance < MAX_DIST)
     {
-        vec3 collisionPoint = camera.position + camera.direction * distance;
+        vec3 collisionPoint = camera.position + camera.direction * colInfo.distance;
 
         col = vec4(vec3(getLight(collisionPoint)), 1.);
+
+        col = colInfo.color * col;
     }
 
     fragColor = col;
