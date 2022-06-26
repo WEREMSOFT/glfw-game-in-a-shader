707b3af7a8fa88f5d73076cb0b73bfc03d400d34
WEREMSOFT <pablo.weremczuk@gmail.com>
2022-06-26 16:07:28 +0200

    fixed windows build

--- build.bat
+++ build.bat
@@ -5,7 +5,15 @@ pushd src
 cl -Zi ^
    /std:c++17 ^
    /I ..\..\libs\glfw\include ^
-    main.cpp ^
+   /I ..\..\libs\glfw\imgui ^
+   main.cpp ^
+   ..\libs\imgui\imgui.cpp^
+   ..\libs\imgui\imgui_demo.cpp^
+   ..\libs\imgui\imgui_draw.cpp^
+   ..\libs\imgui\imgui_impl_glfw.cpp^
+   ..\libs\imgui\imgui_impl_opengl3.cpp^
+   ..\libs\imgui\imgui_tables.cpp^
+   ..\libs\imgui\imgui_widgets.cpp^
     ..\..\libs\glfw\lib-vc2022\glfw3dll.lib ^
     ..\..\libs\glfw\lib-vc2022\glfw3_mt.lib ^
     ..\..\libs\glew\lib\Release\x64\glew32.lib ^
